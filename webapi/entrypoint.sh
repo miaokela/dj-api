cb7060b616207ca890583a41f09c897731ca72b2
miaokl <2972799448@qq.com>
2024-05-11 11:51:36 +0800

    ✨ celery日志

--- webapi/entrypoint.sh
+++ webapi/entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # 启动 Django 服务
-uwsgi --http :8000 --module webapi.wsgi &
+uwsgi --http :8000 --module your_project.wsgi &
 
-# 启动 Celery worker
-celery -A webapi worker --loglevel=info &
+# 启动 Celery worker，并将日志输出到指定文件
+celery -A webapi worker --loglevel=info >> /var/log/celery_worker.log 2>&1 &
 
-# 启动 Celery beat
-celery -A webapi beat --scheduler django_celery_beat.schedulers:DatabaseScheduler --loglevel=info &
+# 启动 Celery beat，并将日志输出到指定文件
+celery -A webapi beat --scheduler django_celery_beat.schedulers:DatabaseScheduler --loglevel=info >> /var/log/celery_beat.log 2>&1 &
 
-# 启动 Flower
-celery -A webapi flower --address=0.0.0.0 --port=5555 &
+# 启动 Flower，并将日志输出到指定文件
+celery -A webapi flower --address=0.0.0.0 --port=5555 >> /var/log/celery_flower.log 2>&1 &
 
 # 等待所有后台任务完成
 wait
